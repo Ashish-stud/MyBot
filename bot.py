2c3d4cb0dfa3037ce36a5e507626b4a516dbc99e
ASHISH SAXENA <ashish.043825@tmu.ac.in>
2025-01-15 01:30:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 # Your bot token
 BOT_TOKEN = "7547766289:AAFNJ1PLTxAgU_skkTcPFtbQLZEzHdwHOHA"
@@ -10,14 +11,14 @@ WEB_APP_URL = "https://serene-bombolone-16362f.netlify.app/"
 # Function to handle the /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.message.from_user.first_name  # Get user's first name
+    # Create the inline button
+    button = InlineKeyboardButton("Click Me To See", web_app={"url": WEB_APP_URL})
+    keyboard = InlineKeyboardMarkup([[button]])
+    
     # Send a personalized greeting message with an inline button
     await update.message.reply_text(
         f"Hello {user_name}!\nWelcome to FamXExclusive Bot!\nTap the button below to see our offers!",
-        reply_markup={
-            "inline_keyboard": [
-                [{"text": "Click Me To See", "web_app": {"url": WEB_APP_URL}}]
-            ]
-        }
+        reply_markup=keyboard
     )
 
 # Main function to run the bot
